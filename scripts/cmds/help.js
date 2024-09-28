61a322b2b3145cf1f6cc3269741924818dc0ad3c
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-09-28 22:18:11 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | Goat Bot V2 ]";
+const doNotDelete = "[ 🐐 | hêîṣ ɓr̃õƙêñ 💔💜 ]";
 /**
 * @author NTKhang
 * @author: do not delete it
@@ -406,4 +406,4 @@ function cropContent(content, max) {
 		content = content + "...";
 	}
 	return content;
-}
+				}

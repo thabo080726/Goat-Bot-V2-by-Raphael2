4fb863a898461461c609b493004243be7538ccb7
Isaiah-ilom <139964528+Isaiah-ilom@users.noreply.github.com>
2024-10-04 17:47:46 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | Raphael scholar ]";
+const doNotDelete = "[ 🐐 | Raphael scholar😃 ]";
 /**
 * @author NTKhang
 * @author: do not delete it
